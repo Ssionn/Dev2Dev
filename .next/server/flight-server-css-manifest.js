8e2076881d606e95b72d4f8272a47900849f76b5
astronomx <unkownusernotfound@protonmail.com>
2023-01-26 13:42:50 +0100

    Deleted .next and node_modules

--- .next/server/flight-server-css-manifest.js
+++
@@ -1,7 +0,0 @@
-self.__RSC_CSS_MANIFEST={
-  "__entry_css_mods__": {
-    "C:\\xampp\\htdocs\\MindSpace project\\Dev2Dev\\app\\login-page\\page": [],
-    "C:\\xampp\\htdocs\\MindSpace project\\Dev2Dev\\app\\topics\\faq\\page": [],
-    "C:\\xampp\\htdocs\\MindSpace project\\Dev2Dev\\app\\page": []
-  }
-}
